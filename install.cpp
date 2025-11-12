837b500de5b55af856139b28cd9d561c7e3aa563
SergantDornan <microsoftvis@gmail.com>
2025-11-12 17:25:30 +0300

    fix

--- install.cpp
+++ install.cpp
@@ -4,11 +4,8 @@
 
 const std::string name = "setssh_to_raspberry";
 
-int main(){
+int main(int argc, char* argv[]){
 
-
-	auto self = getPathByAlias(name);
-    if(self.size() == 0) return 0;
     auto belder = getPathByAlias("belder");
     if(belder.size() == 0) {
             std::cout << "================ ERROR ================" << std::endl;
@@ -17,12 +14,21 @@ int main(){
             return 1;
         }
 	const std::string oldline = "const std::string connect = \"connect.sh\";";
-    const std::string newline = "const std::string connect = \"" + getFolder(self[0]) + "/connect.sh\";";
-    const std::string mainFile = getFolder(self[0]) + "/main.cpp";
+    const std::string newline = "const std::string connect = \"" + cwd() + "/connect.sh\";";
+    const std::string mainFile = "main.cpp";
     rewriteLine(mainFile, oldline, newline);
-    std::string cmd = belder[0] + " main.cpp -o " + name + " -c " + getFolder(self[0]);
+    std::string cmd = belder[0] + " main.cpp -o " + name;
     system(cmd.c_str());
     rewriteLine(mainFile, newline, oldline);
+    auto self = getPathByAlias(name);
+    if(self.size() == 0) {
+        manage_alias(name, {"add_alias"});
+        self = getPathByAlias(name);
+        if(self.size() == 0) {
+            std::cout << "ERROR, cannot find self" << std::endl;
+            return 1;
+        }
+    }
     cmd = belder[0] + " install.cpp -o install -c " + getFolder(self[0]);
     system(cmd.c_str());
     return 0;
